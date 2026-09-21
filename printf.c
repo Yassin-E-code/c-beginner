54be5f5971b163106a365bd6c828ac213c0c271c
Yassin-E-code <yelidrissi205@gmail.com>
2026-09-21 14:16:25 +0000

    if statment

--- printf.c
+++ printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-//just include the standard input output library to use the printf function 
-//then we define the main function which is the entry point of the program
+// the if statement is like a puting a condtion to run a block of code, if the condition is true, the code inside the block will be executed
 int main() {
-    printf("Hello, World!\n");
+    //number 1 stands for true, so the code inside the block will be executed
+    // while 0 stands for false, so the code inside the block will not be executed
+    // if u run the code the output will be "Hello, World! ,true" because the condition is true, while the second block of code will not be executed because the condition is false
+    if (1) {
+        printf("Hello, World! ,true\n");
+    }
+    if (0) {
+        printf("Hello, World! ,false\n");
+    }
+
     return 0;
 }
