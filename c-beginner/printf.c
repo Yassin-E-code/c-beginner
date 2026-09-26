6e6e1bd7abed8d7c72c2604b2445641062dcef8c
Yassin-E-code <yelidrissi205@gmail.com>
2026-09-26 16:53:14 +0000

    scanf

---
+++ c-beginner/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//just include the standard input output library to use the printf function 
+//then we define the main function which is the entry point of the program
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
