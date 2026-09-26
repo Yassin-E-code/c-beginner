5daeadabe5b40e74d6435a0f7daf903e31014ae8
Yassin-E-code <yelidrissi205@gmail.com>
2026-09-26 16:30:24 +0000

    strings

--- strings.c
+++ strings.c
@@ -15,6 +15,13 @@ int main() {
     
     // Print the length of the string
     printf("Length of the string: %d\n", length);
-    
+    // Modify the string
+    str[7] = 'C';
+    printf("Modified string: %s\n", str);
+    //string comparisons can be done using the strcmp function from the string.h library. It compares two strings lexicographically and returns an integer value indicating their relationship. If the first string is less than the second, it returns a negative value; if they are equal, it returns zero; and if the first string is greater, it returns a positive value.
+    char str1[] = "Hello";
+    char str2[] = "World";
+    int comparison = strcmp(str1, str2);
+    printf("Comparison result: %d\n", comparison);
     return 0;
 }
